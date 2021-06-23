888a00c6f02e3ca08bf49e005e63e653bb3c6288
Sajeel-Bongale <sajeelbongale@gmail.com>
2021-06-23 16:26:02 +0530

    Adds NewExpense component for adding new expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
 import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import {useState} from "react";
 
 const ExpenseItem = (props) => {
+
+    let [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated')
+    }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>
-                    {props.title}
+                    {title}
                 </h2>
                 <div className="expense-item__price">
                     ${props.amount}
                 </div>
+                <button onClick={clickHandler}>Update</button>
             </div>
         </Card>
     );
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import './NewExpense.css'
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = function () {
+
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    )
+}
+
+export default NewExpense;
